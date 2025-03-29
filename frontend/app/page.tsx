5e42a7b8cacff9310fab3904f1d754d3ce35792a
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-03-29 08:56:45 +0530

    Started on the front end part

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-7xl text-red-950">Hello</h1>
+      <Button>Button 1</Button>
+      <Button variant="outline">Button 2</Button>
+    </div>
+  )
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border) / <alpha-value>)",
+        input: "hsl(var(--input) / <alpha-value>)",
+        ring: "hsl(var(--ring) / <alpha-value>)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
+        primary: {
+          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+        },
+        /* ...repeat for secondary, destructive, etc. */
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
