b6728492b30f24b3cb4f75838cd0ae6d8aa8f56d
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-02 23:11:32 +0530

    Created the Forgot Password Page

---
+++ frontend/app/auth/forget-password/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ForgetPassword from '@/components/Auth/ForgetPassword'
+const Forgetpassword = () => {
+  return (<>
+  <ForgetPassword />
+  </>
+  )
+}
+
+export default Forgetpassword;
