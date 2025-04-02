976cebd0ec4ad1bfaf8665cf8bc48b2e5301994a
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-02 23:18:04 +0530

    Home Page

--- frontend/components/Home/home.tsx
+++ frontend/components/Home/home.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
 
-const home = () => {
-  return (
-    <div>home</div>
-  )
-}
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
-export default home;
+const Home = () => {
+  const user = useSelector((state: RootState) => state?.auth?.user);
+
+  console.log("User:", user || "No user data available");
+
+  return <div>Home</div>;
+};
+
+export default Home;
