4135af57518ff226064155842e22fb1fe936e1ee
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-02 23:16:06 +0530

    Code for SignUp Page

--- frontend/components/Auth/Signup.tsx
+++ frontend/components/Auth/Signup.tsx
@@ -1,7 +1,59 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import PasswordInput from "./PasswordInput";
+import LoadingButton from "../Helper/loadingButton";
+import Link from "next/link";
+import { ChangeEvent } from "react";
+import { BASE_API_URL } from "@/server";
+import axios from "axios";
+import { handleAuthRequest } from "../utils/apiRequest";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthUser } from "@/store/authSlice";
+import { useRouter } from "next/navigation";
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 const Signup = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState<FormData>({
+    username: "", // Fixed: Was incorrectly set to 0000
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const signupReq = async () =>
+      await axios.post(`${BASE_API_URL}/users/signup`, formData, {
+        withCredentials: true,
+      });
+    const result = await handleAuthRequest(signupReq, setIsLoading);
+
+    if (result) {
+      dispatch(setAuthUser(result.data.data.user));
+      toast.success(result.data.message);
+      router.push("/auth/verify");
+      //TODO's
+      //1.Redirect to Home Page
+      //2.Add our user to Redux store(to store the user details when we reload the
+      //page for that we will be using the Redux-Persist)
+    }
+  };
+
   return (
     <div className="w-full h-screen overflow-hidden">
       <div className="grid grid-cols-1 lg:grid-cols-7 gap-8">
@@ -12,7 +64,7 @@ const Signup = () => {
             alt="signup"
             width={1000}
             height={1000}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover scale-110"
           />
         </div>
 
@@ -21,7 +73,10 @@ const Signup = () => {
             Sign Up with <span className="text-green-600">LinkUp SRMAP</span>
           </h1>
 
-          <form className="block w-[90%] sm:w-[80%] md:w-[60%] lg:w-[90%] xl:w-[80%]">
+          <form
+            onSubmit={handleSubmit}
+            className="block w-[90%] sm:w-[80%] md:w-[60%] lg:w-[90%] xl:w-[80%]"
+          >
             {/* Username Field */}
             <div className="mb-4">
               <label htmlFor="username" className="font-semibold mb-2 block">
@@ -33,6 +88,8 @@ const Signup = () => {
                 id="username"
                 placeholder="Username"
                 className="px-4 py-3 bg-gray-200 rounded-lg w-full block outline-none"
+                value={formData.username}
+                onChange={handleChange}
               />
             </div>
             {/* Email Field */}
@@ -46,10 +103,48 @@ const Signup = () => {
                 id="email"
                 placeholder="Email Address"
                 className="px-4 py-3 bg-gray-200 rounded-lg w-full block outline-none"
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
             {/* Password */}
+            <div className="mb-4">
+              <PasswordInput
+                label="Password"
+                name="password"
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            {/* Confirm Password */}
+            <div className="mb-4">
+              <PasswordInput
+                label="Confirm Password"
+                name="passwordConfirm"
+                placeholder="Confirm Password"
+                value={formData.passwordConfirm}
+                onChange={handleChange}
+              />
+            </div>
+            {/* Submit Button */}
+            <LoadingButton
+              size={"lg"}
+              className="w-full mt-3"
+              type="submit"
+              isLoading={isLoading}
+            >
+              Sign Up Now
+            </LoadingButton>
           </form>
+          <h1 className="mt-4 text-lg text-gray-800">
+            Already have an account ?{" "}
+            <Link href="/auth/login">
+              <span className="text-blue-800 underline cursor-pointer font-medium">
+                Login Here
+              </span>{" "}
+            </Link>
+          </h1>
         </div>
       </div>
     </div>
