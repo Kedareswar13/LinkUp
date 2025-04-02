6c5c9dc75364bc107d50a0abfdc1e53c4d07c36d
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-02 23:21:38 +0530

    Normal Modifications

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
 import "./globals.css";
-
+import { Toaster } from "@/components/ui/sonner";
+import ClientProvider from "@/HOC/ClientProvider";
 const font = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
@@ -24,7 +25,10 @@ export default function RootLayout({
       <body
         className={`${font.className} antialiased`}
       >
-        {children}
+        <ClientProvider>
+          {children}
+          <Toaster />
+        </ClientProvider>
       </body>
     </html>
   );
