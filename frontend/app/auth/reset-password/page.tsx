a46f6c15d64057a851783b82dee4d69266060b15
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-03 21:12:28 +0530

    Created a Reset Password page

---
+++ frontend/app/auth/reset-password/page.tsx
@@ -0,0 +1,13 @@
+import ResetPassword from '@/components/Auth/ResetPassword'
+import React from 'react'
+
+
+const ResetPasswordPage = () => {
+  return (
+    <>
+    <ResetPassword/>
+    </>
+  )
+}
+
+export default ResetPasswordPage
