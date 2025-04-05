37bb05b4979b316df633ecb885776317aab83596
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-05 21:06:00 +0530

    Finished the home page

--- frontend/components/Home/home.tsx
+++ frontend/components/Home/home.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import LeftSidebar from "./LeftSidebar";
 import Feed from "./Feed";
 import RightSidebar from "./RightSidebar";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "../ui/sheet";
+import { MenuIcon } from "lucide-react";
 
 const Home = () => {
   return (
@@ -12,6 +14,18 @@ const Home = () => {
       <LeftSidebar/>
     </div>
     <div className="flex-1 md:ml-[20%] overflow-y-auto">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon/>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetTitle></SheetTitle>
+            <SheetDescription></SheetDescription>
+            <LeftSidebar/>
+          </SheetContent>
+        </Sheet>
+      </div>
       <Feed/>
     </div>
     <div className="w-[30%] pt-8 px-6 lg:block hidden">
