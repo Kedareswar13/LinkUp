be4e64ef97908844f19633a3db7a17689fcb4ead
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-06 22:03:08 +0530

    Modified the next.config.js and types.d.ts files

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,37 +1,39 @@
 export interface User {
+  _id: string;
+  username: string;
+  email: string;
+  password?: string;
+  profilePicture?: string;
+  bio?: string;
+  followers: string[];
+  following: string[];
+  posts: Post[];
+  savedPosts: string[] | Post[];
+  isVerified: boolean;
+}
+
+export interface Comment {
+  _id: string;
+  text: string;
+  user: {
     _id: string;
     username: string;
-    email: string;
-    password?: string;
-    profilePicture?: string;
-    bio?: string;
-    followers: string[];
-    following: string[];
-    posts: Post[];
-    savedPosts: string[] | Post[];
-    isVerified: boolean;
-  }
+    profilePicture: string;
+  };
+  createdAt: string;
+}
 
-  export interface Comment {
-    _id: string;
-    text: string;
-    user: {
-      _id: string;
-      username: string;
-      profilePicture: string;
-    };
-    createdAt: string;
-  }
-  
-  export interface Post {
-    _id: string;
-    caption: string;
-    image?: {
-      url: string;
-      publicId: string;
-    };
-    user: User | undefined;
-    likes: string[];
-    comments: Comment[];
-    createdAt: string;
-  }
+export interface Post {
+  _id: string;
+  caption: string;
+  media?: {
+    url: string;
+    publicId: string;
+    type: "image" | "video";
+  };
+  user: User;
+  likes: string[];
+  comments: Comment[];
+  createdAt: string;
+  updatedAt?: string; // Optional if using Mongoose timestamps
+}
