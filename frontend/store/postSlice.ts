4051263f07546c4714ff1f063f0ab0a37656782f
Kedareswar13 <kedareswar_pattapu@srmap.edu.in>
2025-04-06 22:02:32 +0530

    Modified the PostSlice file

--- frontend/store/postSlice.ts
+++ frontend/store/postSlice.ts
@@ -10,7 +10,7 @@ const initialState: PostState = {
 };
 
 const postSlice = createSlice({
-  name: 'post',
+  name: "post",
   initialState,
   reducers: {
     setPost: (state, action: PayloadAction<Post[]>) => {
@@ -20,26 +20,32 @@ const postSlice = createSlice({
       state.posts.unshift(action.payload);
     },
     deletePost: (state, action: PayloadAction<string>) => {
-        state.posts = state.posts.filter((post) => post._id !== action.payload);
+      state.posts = state.posts.filter((post) => post._id !== action.payload);
     },
-    likeOrDislike: (state, action: PayloadAction<{postId : string; userId : string}>) => {
-        const post = state.posts.find((post)=>post._id === action.payload.postId);
-        if(post){
-            if(post.likes.includes(action.payload.userId)){
-                post.likes = post.likes.filter((id) => id !== action.payload.userId)
-            }else{
-                post.likes.push(action.payload.userId);
-            }
+    likeOrDislike: (
+      state,
+      action: PayloadAction<{ postId: string; userId: string }>
+    ) => {
+      const post = state.posts.find((post) => post._id === action.payload.postId);
+      if (post) {
+        if (post.likes.includes(action.payload.userId)) {
+          post.likes = post.likes.filter((id) => id !== action.payload.userId);
+        } else {
+          post.likes.push(action.payload.userId);
         }
+      }
     },
-    addComment: (state, action: PayloadAction<{postId:string,comment:Comment}>) => {
-      const post = state.posts.find((post)=>post._id === action.payload.postId);
-      if(post){
-        post.comments.push(action.payload.comment)
+    addComment: (
+      state,
+      action: PayloadAction<{ postId: string; comment: Comment }>
+    ) => {
+      const post = state.posts.find((post) => post._id === action.payload.postId);
+      if (post) {
+        post.comments.push(action.payload.comment);
       }
     },
   },
 });
 
-export const { setPost, addPost, deletePost , likeOrDislike , addComment} = postSlice.actions;
+export const { setPost, addPost, deletePost, likeOrDislike, addComment } = postSlice.actions;
 export default postSlice.reducer;
